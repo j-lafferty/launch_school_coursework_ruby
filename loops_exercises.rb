b43bb9bfbaa141c463307ec9b48de1f62b7d66b9
James Lafferty <james@j-lafferty.net>
2020-06-19 14:55:32 -0700

    Add solution for that's odd

--- loops_exercises.rb
+++ loops_exercises.rb
@@ -45,3 +45,15 @@ until count == numbers.length do
   puts numbers[count]
   count += 1
 end
+
+=begin
+The code below shows an example of a for loop. Modify the code so that it only outputs i if i is an odd number.
+
+for i in 1..100
+  puts i
+end
+=end
+for i in 1..100 
+  puts i if i.odd?
+end
+  
