96eb00acf53e7aa0e95542570e187c165f5a362a
James Lafferty <james@j-lafferty.net>
2020-06-19 14:49:24 -0700

    Add solution for print until

--- loops_exercises.rb
+++ loops_exercises.rb
@@ -32,3 +32,16 @@ until count > 10
   puts count
   count += 1
 end
+
+=begin
+Given the array of several numbers below, use an until loop to print each number.
+
+numbers = [7, 9, 13, 25, 18]
+=end
+numbers = [7, 9, 13, 25, 18]
+count = 0
+
+until count == numbers.length do
+  puts numbers[count]
+  count += 1
+end
