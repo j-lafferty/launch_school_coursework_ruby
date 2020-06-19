544d03ae8f452805fc01fa674c4c4ce7f841aa02
James Lafferty <james@j-lafferty.net>
2020-06-19 14:43:24 -0700

    Add solution for print while

---
+++ loops_exercises.rb
@@ -0,0 +1,17 @@
+=begin
+Using a while loop, print 5 random numbers between 0 and 99. The code below shows an example of how to begin solving this exercise.
+
+numbers = []
+
+while <condition>
+  # ...
+end
+=end
+
+numbers = []
+
+while numbers.length < 5
+  numbers << rand(99)
+  # ...
+end
+p numbers
