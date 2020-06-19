7ed6aac336a3fed9305ba79a8fd229a9217a1a98
James Lafferty <james@j-lafferty.net>
2020-06-19 16:47:03 -0700

    Add solution for only even

--- loops_2_exercises.rb
+++ loops_2_exercises.rb
@@ -102,3 +102,23 @@ end
   puts index
   break if index == 2
 end
+
+=begin
+Using next, modify the code below so that it only prints even numbers.
+
+number = 0
+
+until number == 10
+  number += 1
+  puts number
+end
+=end
+number = 0
+
+until number == 10
+  number += 1
+  next if number.odd?
+  puts number
+end
+
+
