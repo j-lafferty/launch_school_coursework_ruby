806ee55a5db43eb72d0140b0f82e2cb7ae046a8a
James Lafferty <james@j-lafferty.net>
2020-06-20 13:12:12 -0700

    Add solution for random sentence

--- methods_exercises.rb
+++ methods_exercises.rb
@@ -110,3 +110,31 @@ end
 puts add(2, 2) == 4
 puts add(5, 4) == 9
 puts multiply(add(2, 2), add(5, 4)) == 36
+
+=begin 
+The variables below are both assigned to arrays. The first one, names, contains a list of names. The second one, activities, contains a list of activities. Write the methods name and activity so that they each take the appropriate array and return a random value from it. Then write the method sentence that combines both values into a sentence and returns it from the method.
+
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+puts sentence(name(names), activity(activities))
+Example output:
+
+George went walking today!
+=end
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+def name(names)
+  names.sample
+end
+
+def activity(activities)
+  activities.sample
+end
+
+def sentence(name, activities)
+  "#{name} went #{activities} today!"
+end
+
+puts sentence(name(names), activity(activities))
