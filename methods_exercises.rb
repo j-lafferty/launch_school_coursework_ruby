6f34cde496d4c1fccf039d44beba1ebe2838641d
James Lafferty <james@j-lafferty.net>
2020-06-20 12:44:06 -0700

    Add solution for day or night

--- methods_exercises.rb
+++ methods_exercises.rb
@@ -26,3 +26,27 @@ def greet
 end
 
 greet
+
+=begin
+Using the following code, write a method called car that takes two arguments and prints a string containing the values of both arguments.
+
+car('Toyota', 'Corolla')
+=end
+def car(make, model)
+  puts "#{make} #{model}"
+end
+
+car('Toyota', 'Corolla')
+
+=begin
+The variable below will be randomly assigned as true or false. Write a method named time_of_day that, given a boolean as an argument, prints "It's daytime!" if the boolean is true and "It's nighttime!" if it's false. Pass daylight into the method as the argument to determine whether it's day or night.
+
+daylight = [true, false].sample
+=end
+def time_of_day(x)
+  puts x ? "It's daytime!" : "It's nighttime!"
+end
+
+daylight = [true, false].sample
+
+time_of_day(daylight)
