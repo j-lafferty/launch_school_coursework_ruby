13df3621d5ee30e8bb64bee51f52ef59a276e4ef
James Lafferty <james@j-lafferty.net>
2020-06-22 15:41:09 -0700

    Add program main loop and verify user enters valid number

--- rb101_programming_foundations/lesson_2_calculator.rb
+++ rb101_programming_foundations/lesson_2_calculator.rb
@@ -7,32 +7,62 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def valid_number?(num)
+  num.to_i != 0
+end
+
+number1 = ''
+number2 = ''
+operation = ''
+
 prompt("Welcome to Calculator!")
-prompt("Please enter the firt number")
-number1 = gets.chomp
 
-prompt("Please enter the second number")
-number2 = gets.chomp
+loop do # main program loop
+  loop do
+    prompt("Please enter the firt number")
+    number1 = gets.chomp
+    
+    break if valid_number?(number1)
+    prompt("Hmm... that doesn't look like a valid number.")
+  end
 
-prompt("What operation would you like to perform on these two numbers?")
-prompt("--Add")
-prompt("--Subtract")
-prompt("--Multiply")
-prompt("--Divide")
-operation = gets.chomp.downcase
+  loop do
+    prompt("Please enter the second number")
+    number2 = gets.chomp
+    
+    break if valid_number?(number2)
+    prompt("Hmm... that doesn't look like a valid number.")
+  end
 
-result = case operation
-         when 'add' then number1.to_i + number2.to_i
-         when 'subtract' then number1.to_i - number2.to_i
-         when 'multiply' then number1.to_i * number2.to_i
-         when 'divide' then number1.to_f / number2.to_f
+  prompt("What operation would you like to perform on these two numbers?")
+  prompt("--Add")
+  prompt("--Subtract")
+  prompt("--Multiply")
+  prompt("--Divide")
+  operation = gets.chomp.downcase
+
+  result = case operation
+          when 'add' then number1.to_i + number2.to_i
+          when 'subtract' then number1.to_i - number2.to_i
+          when 'multiply' then number1.to_i * number2.to_i
+          when 'divide' then number1.to_f / number2.to_f
+  end
+
+  sign = case operation
+        when 'add' then '+'
+        when 'subtract' then '-'
+        when 'multiply' then '*'
+        when 'divide' then '/'
+  end
+
+  prompt("#{number1} #{sign} #{number2} = #{result}")
+  prompt("----------------")
+  prompt("")
+  prompt("Would you like to perform another calculation?")
+  prompt("Press 'Y' to calculate again.")
+  prompt("Press 'N' to cancle.")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-sign = case operation
-       when 'add' then '+'
-       when 'subtract' then '-'
-       when 'multiply' then '*'
-       when 'divide' then '/'
-end
-
-prompt("#{number1} #{sign} #{number2} = #{result}")
+prompt("Thanks for using the Calculator!")
