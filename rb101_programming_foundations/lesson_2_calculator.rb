fe6cce6b1ecd790a7ef7d540942dda103a42180f
James Lafferty <james@j-lafferty.net>
2020-06-22 15:04:47 -0700

    Add propmt method

--- rb101_programming_foundations/lesson_2_calculator.rb
+++ rb101_programming_foundations/lesson_2_calculator.rb
@@ -3,25 +3,29 @@
 # peform the operation on the two number
 # output the results
 
-puts "Welcome to Calculator!"
-puts "Please enter the firt number"
+def prompt(message)
+  puts "=> #{message}"
+end
+
+prompt("Welcome to Calculator!")
+prompt("Please enter the firt number")
 number1 = gets.chomp
 
-puts "Please enter the second number"
+prompt("Please enter the second number")
 number2 = gets.chomp
 
-puts "What operation would you like to perform on these two numbers?"
-puts "--Add"
-puts "--Subtract"
-puts "--Multiply"
-puts "--Divide"
+prompt("What operation would you like to perform on these two numbers?")
+prompt("--Add")
+prompt("--Subtract")
+prompt("--Multiply")
+prompt("--Divide")
 operation = gets.chomp.downcase
 
 case operation
-when 'add' then puts "#{number1} + #{number2} = #{number1.to_i + number2.to_i}"
-when 'subtract' then puts "#{number1} - #{number2} = #{number1.to_i - number2.to_i}"
-when 'multiply' then puts "#{number1} * #{number2} = #{number1.to_i * number2.to_i}"
-when 'divide' then puts "#{number1} / #{number2} = #{number1.to_f / number2.to_f}"
-else puts "Incorrect operation selection"
+when 'add' then prompt("#{number1} + #{number2} = #{number1.to_i + number2.to_i}")
+when 'subtract' then prompt("#{number1} - #{number2} = #{number1.to_i - number2.to_i}")
+when 'multiply' then prompt("#{number1} * #{number2} = #{number1.to_i * number2.to_i}")
+when 'divide' then prompt("#{number1} / #{number2} = #{number1.to_f / number2.to_f}")
+else prompt("Incorrect operation selection")
 end
 
