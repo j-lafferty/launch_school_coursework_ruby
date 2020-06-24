0a6f1d98102a5396423ef5da10039fa6f3a8b3dc
James Lafferty <james@j-lafferty.net>
2020-06-24 10:02:01 -0700

    Add valid choices and ask for user input

--- rb101_programming_foundations/lesson_2_rock_paper_scissors.rb
+++ rb101_programming_foundations/lesson_2_rock_paper_scissors.rb
@@ -6,3 +6,14 @@ the computer makes a choice
 the winner is displayed
 =end
 
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+choice = gets.chomp
+
+computer_choice = VALID_CHOICES.sample
+
