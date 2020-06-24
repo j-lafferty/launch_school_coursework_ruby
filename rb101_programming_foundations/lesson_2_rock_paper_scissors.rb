9d07770231ff8b2ef51befb5a57b7ac69a0150f2
James Lafferty <james@j-lafferty.net>
2020-06-24 09:16:49 -0700

    Initial commit for rock paper scissors

---
+++ rb101_programming_foundations/lesson_2_rock_paper_scissors.rb
@@ -0,0 +1,8 @@
+=begin 
+In this assignment, we'll build a Rock Paper Scissors game. The game flow should go like this:
+
+the user makes a choice
+the computer makes a choice
+the winner is displayed
+=end
+
