2a9366db3d316c46d25d8138e190567e67eb4989
James Lafferty <james@j-lafferty.net>
2020-06-25 09:36:36 -0700

    Add lesson 3, easy 1, question 3 practice problem

---
+++ rb101_programming_foundations/lesson_3/practice_problems_easy_1_q3.rb
@@ -0,0 +1,9 @@
+=begin 
+Question 3
+Replace the word "important" with "urgent" in this string:
+=end
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.gsub!("important", "urget")
+
+p advice
