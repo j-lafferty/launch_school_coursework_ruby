404b5d5541393bf4b5ff051b276679af7ff92567
James Lafferty <james@j-lafferty.net>
2020-06-24 18:28:42 -0700

    Add player and computer score

--- rb101_programming_foundations/lesson_2_rock_paper_scissors.rb
+++ rb101_programming_foundations/lesson_2_rock_paper_scissors.rb
@@ -26,6 +26,11 @@ VALID_INPUT = {
   spock: ['spock', 'sp']
 }
 
+scores = {
+  player: 0,
+  computer: 0
+}
+
 def prompt(message)
   puts("=> #{message}")
 end
@@ -51,27 +56,40 @@ def round(player, computer)
   end
 end
 
+# update player and computer score
+def update_score(player, computer, scores)
+  if win?(player, computer)
+    scores[:player] += 1
+  elsif win?(computer, player)
+    scores[:computer] += 1
+  end
+end
+
 # main game loop
 loop do
-  choice = ''
+  player_choice = ''
 
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}\n\
     Abbreviate: 'sp' for spock, 'sc' for scissors, 'r' for rock, etc.")
-    choice = gets.chomp
+    player_choice = gets.chomp
 
-    choice = standardize_input(choice)
+    player_choice = standardize_input(player_choice)
 
-    break if VALID_CHOICES.include?(choice)
+    break if VALID_CHOICES.include?(player_choice)
     prompt("That's not a valid choice!")
   end
 
   computer_choice = VALID_CHOICES.sample
 
   prompt("")
-  prompt("You chose #{choice}; Computer chose #{computer_choice}")
+  prompt("You chose #{player_choice}; Computer chose #{computer_choice}")
 
-  round(choice, computer_choice)
+  round(player_choice, computer_choice)
+  update_score(player_choice, computer_choice, scores)
+
+  prompt("")
+  prompt("Player score: #{scores[:player]}; Computer score: #{scores[:computer]}")
 
   prompt("")
   prompt("Play another round?")
