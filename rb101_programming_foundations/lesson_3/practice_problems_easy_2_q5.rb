329d0173a76d2389f7bdd4c5aa2939c0c65a0d07
James Lafferty <james@j-lafferty.net>
2020-06-25 11:24:37 -0700

    Add solution for easy 2, question 5

---
+++ rb101_programming_foundations/lesson_3/practice_problems_easy_2_q5.rb
@@ -0,0 +1,7 @@
+=begin
+Question 5
+Show an easier way to write this array:
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+=end
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
