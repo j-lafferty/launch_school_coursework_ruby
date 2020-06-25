0989851e586b1706f76a07002030e84a1ddebe30
James Lafferty <james@j-lafferty.net>
2020-06-25 11:00:57 -0700

    Add solution for easy 2, question 1

---
+++ rb101_programming_foundations/lesson_3/practice_problems_easy_2_q1.rb
@@ -0,0 +1,14 @@
+=begin
+Question 1
+In this hash of people and their age,
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+see if "Spot" is present.
+
+Bonus: What are two other hash methods that would work just as well for this solution?
+=end
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+ages.include?("Spot")
+ages.key?("Spot")
+ages.member?("Spot")
