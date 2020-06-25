81b8ba3f835c713be2805df10bc60386c47c8d8b
James Lafferty <james@j-lafferty.net>
2020-06-25 11:25:56 -0700

    Add solution for easy 2, question 6

---
+++ rb101_programming_foundations/lesson_3/practice_problems_easy_2_q6.rb
@@ -0,0 +1,11 @@
+=begin
+Question 6
+How can we add the family pet "Dino" to our usual array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+=end
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones << "Dino"
+
+p flintstones
