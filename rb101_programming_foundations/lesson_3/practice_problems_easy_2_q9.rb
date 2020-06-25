143d2eb217d478410f6b2dc1815a2202a4c36a8a
James Lafferty <james@j-lafferty.net>
2020-06-25 11:42:06 -0700

    Add solution for easy 2, question 9

---
+++ rb101_programming_foundations/lesson_3/practice_problems_easy_2_q9.rb
@@ -0,0 +1,9 @@
+=begin
+Question 9
+Write a one-liner to count the number of lower-case 't' characters in the following string:
+
+statement = "The Flintstones Rock!"
+=end
+statement = "The Flintstones Rock!"
+
+p statement.count('t')
