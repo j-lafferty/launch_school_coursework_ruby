d6eedd237d6907518bcdbc371642ba940c2b6ec4
James Lafferty <james@j-lafferty.net>
2020-06-26 11:41:41 -0700

    Add method that reflects algorithm explanation

--- rb101_programming_foundations/lesson_4/palindrome_how_to_solve.rb
+++ rb101_programming_foundations/lesson_4/palindrome_how_to_solve.rb
@@ -53,3 +53,22 @@
 #   - reassign `end_substring_idx` to `start_substring_idx += 1`
 # - end outer loop
 # - return `result` array
+
+def substrings(str)
+  result = []
+  start_substring_idx = 0
+  end_substring_idx = start_substring_idx + 1
+
+  loop do
+    break if start_substring_idx == str.size
+    loop do
+      break if end_substring_idx == str.size
+      result << str[start_substring_idx..end_substring_idx]
+      end_substring_idx += 1
+    end
+    start_substring_idx += 1
+    end_substring_idx += 1
+  end
+
+  result
+end
