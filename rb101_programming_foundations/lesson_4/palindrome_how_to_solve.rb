192eab09e40792502d8dd6712788d7f94455d177
James Lafferty <james@j-lafferty.net>
2020-06-26 11:47:28 -0700

    Update pseudocode

--- rb101_programming_foundations/lesson_4/palindrome_how_to_solve.rb
+++ rb101_programming_foundations/lesson_4/palindrome_how_to_solve.rb
@@ -36,23 +36,25 @@
 #  - return the result array
 
 # ALGORITHM:
-# - create an empty array called `result` that will contain all
-#   the required substrings
-# - initialize variable start_substring_idx and assign 0 to it.
-# - initialize variable end_substring_idx and assign value of
-#   start_substring_idx + 1 to it.
-# - Enter loop which will break when start_substring_idx is equal
-#     to str.size
-#   - Within that loop enter another loop that will break if
-#     end_substring_idx is equal to str.size
-#     - append to the result array part of the string from
-#       start_substring_idx to end_substring_idx
-#     - increment `end_substring_idx` by 1.
-#   - end the inner loop
-#   - increment `start_substring_idx` by 1.
-#   - reassign `end_substring_idx` to `start_substring_idx += 1`
-# - end outer loop
-# - return `result` array
+#  substrings method
+#  =================
+#  - create an empty array called `result` which will contain all
+#    the required substrings
+#  - initialize variable start_substring_idx and assign 0 to it.
+#  - initialize variable end_substring_idx and assign value of
+#    start_substring_idx + 1 to it.
+#  - Enter loop which will break when start_substring_idx is equal
+#      to str.size
+#    - Within that loop enter another loop which will break if
+#      end_substring_idx is equal to str.size
+#      - append to the result array part of the string from
+#        start_substring_idx to end_substring_idx
+#      - increment `end_substring_idx` by 1.
+#    - end the inner loop
+#    - increment `start_substring_idx` by 1.
+#    - reassign `end_substring_idx` to `start_substring_idx += 1`
+#  - end outer loop
+#  - return `result` array
 
 def substrings(str)
   result = []
@@ -73,11 +75,21 @@ def substrings(str)
   result
 end
 
-# Check if string is palindrome
-# - Inside the `is_palindrome?` method, check whether the string
-#   value is equal to its reversed value. You can use the
-#   String#reverse method.
+#  is_palindrome? method
+#  =====================
+#  - check whether the string value is equal to its reversed
+#    value. You can use the String#reverse method.
 
 def is_palindrome?(str)
   str == str.reverse
 end
+
+#  palindrome_substrings method
+#  ============================
+#  - initialize a result variable to an empty array
+#  - create an array named substring_arr that contains all of the
+#    substrings of the input string that are at least 2 characters long.
+#  - loop through the words in the substring_arr array.
+#    - if the word is a palindrome, append it to the result
+#      array
+#  - return the result array
