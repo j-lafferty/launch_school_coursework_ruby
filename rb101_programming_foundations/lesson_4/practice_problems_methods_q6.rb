c5a4884ce9c39c926a4c77fcd737fbce7f3fade6
James Lafferty <james@j-lafferty.net>
2020-06-27 09:40:36 -0700

    Add solution for problem 6

---
+++ rb101_programming_foundations/lesson_4/practice_problems_methods_q6.rb
@@ -0,0 +1,8 @@
+# Practice Problem 6
+# What is the return value of the following statement? Why?
+
+['ant', 'bear', 'caterpillar'].pop.size
+# removes and returns the last element
+# then counts the length of the returned element
+# in this case, 'caterpillar' is removed and returned with pop
+# and size then returns 11 as the value
