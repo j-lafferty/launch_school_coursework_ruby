2c505de12c06024aa4df8def9ccc9c3dd4bc2ad5
James Lafferty <james@j-lafferty.net>
2020-06-27 14:26:00 -0700

    Add solution for problem 8

---
+++ rb101_programming_foundations/lesson_4/practice_problems_methods_q8.rb
@@ -0,0 +1,9 @@
+# Practice Problem 8
+# How does take work? Is it destructive? How can we find out?
+
+arr = [1, 2, 3, 4, 5]
+arr.take(2)
+# returns an array of a specified number of elements,
+# starting from index 0
+# non-mutating
+# the above would return [1, 2]
