746d0404074554803c14e531ce7ae26c5fbc5573
James Lafferty <james@j-lafferty.net>
2020-06-27 09:36:45 -0700

    Add solution for problem 5

---
+++ rb101_programming_foundations/lesson_4/practice_problems_methods_q5.rb
@@ -0,0 +1,7 @@
+# Practice Problem 5
+# What does shift do in the following code? How can we find out?
+
+hash = { a: 'ant', b: 'bear' }
+hash.shift
+# shift removes the first key-value pair from the hash
+# returns this key-value pair as an array, [ key, value ]
