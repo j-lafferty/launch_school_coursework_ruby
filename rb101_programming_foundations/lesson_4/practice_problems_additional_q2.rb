e80634adbf8cc373d72284a55e9eae15290f121a
James Lafferty <james@j-lafferty.net>
2020-06-27 18:45:46 -0700

    Add solution for additional problems, q2

---
+++ rb101_programming_foundations/lesson_4/practice_problems_additional_q2.rb
@@ -0,0 +1,7 @@
+# Practice Problem 2
+# Add up all of the ages from the Munster family hash:
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+result =  0
+ages.each_value {|value| result += value}
