c0eb69d6a390096efc55d96c303c43936bb77948
James Lafferty <james@j-lafferty.net>
2020-06-27 18:36:43 -0700

    Add solution for additional problems, q1

---
+++ rb101_programming_foundations/lesson_4/practice_problems_additional_q1.rb
@@ -0,0 +1,7 @@
+# Practice Problem 1
+# Given the array below
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+# Turn this array into a hash where the names are the keys and the values are the positions in the array.
+
+flintstones.to_h {|i| [i, flintstones.index(i)]}
