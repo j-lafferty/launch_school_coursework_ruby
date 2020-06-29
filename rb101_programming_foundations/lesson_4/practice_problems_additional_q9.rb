48b339476d18f45bf3ff2a7304d6f4ebf64f13df
James Lafferty <james@j-lafferty.net>
2020-06-28 19:33:19 -0700

    Add solution for additional problems, q9

---
+++ rb101_programming_foundations/lesson_4/practice_problems_additional_q9.rb
@@ -0,0 +1,27 @@
+# Practice Problem 9
+# As we have seen previously, 
+# we can use some built-in string methods to change the case of a string. 
+# A notably missing method is something provided in Rails, 
+# but not in Ruby itself...titleize. 
+# This method in Ruby on Rails creates a string that has each word capitalized as it would be in a title. 
+
+# For example, the string:
+
+words = "the flintstones rock"
+
+# would be:
+
+words = "The Flintstones Rock"
+# Write your own version of the rails titleize implementation.
+
+titleize = []
+words_arr = words.split
+
+words_arr.each do |word|
+  titleize.push(word.capitalize)
+end
+
+titleize.join(' ')
+
+# shorter version:
+words.split.collect {|word| word.capitalize}.join(' ')
