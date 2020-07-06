8bd9108868240203ecddc1d61bd88500a9f29d42
James Lafferty <james@j-lafferty.net>
2020-07-06 11:40:41 -0700

    Add solution for collections problem 6

---
+++ rb101_programming_foundations/lesson_5/practice_problems_collections_q6.rb
@@ -0,0 +1,19 @@
+# Practice Problem 6
+# One of the most frequently used real-world string properties is that of "string substitution", where we take a hard-coded string and modify it with various parameters from our program.
+
+# Given this previously seen family hash, print out the name, age and gender of each family member:
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+# ...like this:
+
+# (Name) is a (age)-year-old (male or female).
+
+munsters.each do |member, detail|
+  p "#{member} is a #{detail['age']}-year-old #{detail['gender']}."
+end
