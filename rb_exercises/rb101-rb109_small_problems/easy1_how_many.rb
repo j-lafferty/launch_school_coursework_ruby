b184a5a82012e1d3993c0f89397afbdcc00bd474
James Lafferty <james@j-lafferty.net>
2020-08-11 16:53:03 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy1_how_many.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_how_many.rb
@@ -1,9 +1,9 @@
 # Write a method that counts the number of occurrences of each element in a given array.
 
-# vehicles = [
-#   'car', 'car', 'truck', 'car', 'SUV', 'truck',
-#   'motorcycle', 'motorcycle', 'car', 'truck'
-# ]
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'Car', 'truck'
+]
 
 # count_occurrences(vehicles)
 # The words in the array are case-sensitive: 'suv' != 'SUV'. ` Once counted, print each element alongside the number of occurrences.
@@ -15,3 +15,15 @@
 # SUV => 1
 # motorcycle => 2
 
+def count_occurrences(arr)
+  occurrences = {}
+  arr_downcase = arr.map {|i| i.downcase} # case insensitive
+
+  arr_downcase.uniq.each do |i|
+    occurrences[i] = arr_downcase.count(i)
+  end
+  
+  occurrences.each { |item, num| puts "#{item} => #{num}"}
+end
+
+count_occurrences(vehicles)
