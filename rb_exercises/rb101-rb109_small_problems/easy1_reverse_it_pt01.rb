9179c7645fdf0cf336e20553edb265a9ba6a9348
James Lafferty <james@j-lafferty.net>
2020-08-12 15:36:14 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy1_reverse_it_pt01.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_reverse_it_pt01.rb
@@ -1,6 +1,8 @@
 # Write a method that takes one argument, a string, and returns a new string with the words in reverse order.
 
-
+def reverse_sentence(str)
+  str.split.reverse.join(' ')
+end
 
 
 # Examples:
