932e4cfb03a0920c9574613aff5838ccb2bda65b
James Lafferty <james@j-lafferty.net>
2020-08-12 17:05:47 -0700

    Refactor solution

--- rb_exercises/rb101-rb109_small_problems/easy1_reverse_it_pt02.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_reverse_it_pt02.rb
@@ -1,11 +1,10 @@
 # Write a method that takes one argument, a string containing one or more words, and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
 
-# initial solution
 def reverse_words(str)
   words = []
 
-  str.split.each do |word|
-    word.length >= 5 ? words << word.reverse : words << word
+  words << str.split.each do |word|
+    word.length >= 5 ? word.reverse! : word
   end
   
   words.join(' ')
