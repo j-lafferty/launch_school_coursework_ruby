0dc1df7430ec1a0d7dc75ec98b00d0ee5edd21e9
James Lafferty <james@j-lafferty.net>
2020-08-12 17:43:03 -0700

    Add alertnate solution

--- rb_exercises/rb101-rb109_small_problems/easy1_stringy_strings.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_stringy_strings.rb
@@ -13,9 +13,22 @@ def stringy(num)
   str
 end
 
+# alternate solution
+def alt_stringy(num)
+  str = ''
+  num.times {|i| i.even? ? str << '1' : str << '0'}
+  str
+end
+
 # Examples:
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+# The tests above should print true.
+puts ''
+puts alt_stringy(4) == '1010'
+puts alt_stringy(9) == '101010101'
+puts alt_stringy(6) == '101010'
+puts alt_stringy(7) == '1010101'
 # The tests above should print true.
