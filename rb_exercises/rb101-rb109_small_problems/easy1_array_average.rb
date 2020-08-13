c1761996075b0a82008e83e5c3bb4f7f9ffe2ff4
James Lafferty <james@j-lafferty.net>
2020-08-13 09:44:39 -0700

    Add solution for Float, rounded to 2 decimal digits

--- rb_exercises/rb101-rb109_small_problems/easy1_array_average.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_array_average.rb
@@ -4,8 +4,18 @@ def average(arr)
   arr.sum / arr.length
 end
 
+# return as Float, rounded to 2 decimal digits
+def average_float(arr)
+  (arr.sum(0.0) / arr.length).round(2)
+end
+
 # Examples:
 puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
+# The tests above should print true.
+puts ''
+puts average_float([1, 6]) == 3.50 # integer division: (1 + 6) / 2 -> 3
+puts average_float([1, 5, 87, 45, 8, 8]) == 25.67
+puts average_float([9, 47, 23, 95, 16, 52]) == 40.33
 # The tests above should print true.
