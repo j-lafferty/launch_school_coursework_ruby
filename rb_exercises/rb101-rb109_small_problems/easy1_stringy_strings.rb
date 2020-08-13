d8bc8026ea17489855d3c495972c5d1ba24137b5
James Lafferty <james@j-lafferty.net>
2020-08-12 17:41:08 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy1_stringy_strings.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_stringy_strings.rb
@@ -1,6 +1,17 @@
 # Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
 
+# initial solution
+def stringy(num)
+  str = ''
+  i = 1
 
+  while i <= num do
+    i.odd? ? str << '1' : str << '0'
+    i += 1
+  end
+
+  str
+end
 
 # Examples:
 puts stringy(6) == '101010'
