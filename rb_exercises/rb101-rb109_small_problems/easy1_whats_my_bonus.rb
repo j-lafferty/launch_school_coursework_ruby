c25342982e047fbc0364e49e2145a97fae34615a
James Lafferty <james@j-lafferty.net>
2020-08-13 10:41:52 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy1_whats_my_bonus.rb
+++ rb_exercises/rb101-rb109_small_problems/easy1_whats_my_bonus.rb
@@ -1,6 +1,8 @@
 # Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
-
+def calculate_bonus(salary, bonus)
+  bonus ? salary / 2 : 0
+end
 
 # Examples:
 puts calculate_bonus(2800, true) == 1400
