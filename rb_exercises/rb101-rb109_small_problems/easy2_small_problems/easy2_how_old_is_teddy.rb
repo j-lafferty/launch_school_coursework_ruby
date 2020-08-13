150cfbc2d6b4fb3b73f417293b4cd7c23771ef0c
James Lafferty <james@j-lafferty.net>
2020-08-13 16:33:37 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_how_old_is_teddy.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_how_old_is_teddy.rb
@@ -1,6 +1,13 @@
 # Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 200.
 
+def get_age(name)
+  name = 'Teddy' if name.empty?
+  puts "#{name} is #{rand(20..200)} years old!"
+end
 
+puts "Please enter a name: "
+name = gets.chomp
 
+get_age(name)
 # Example Output
 # Teddy is 69 years old!
