e49c48084e7e0cfc1a273b356899143e572eee7a
James Lafferty <james@j-lafferty.net>
2020-08-15 12:01:54 -0700

    Add alternate solution

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_odd_numbers.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_odd_numbers.rb
@@ -1,3 +1,7 @@
 # Print all odd numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
 
+# initial solution
 (1..99).step(2) {|i| puts i}
+
+# alternate solution
+puts (1..99).select(&:i)
