fa32f6a99f8cc792b00d03e1fab4a2930aff96f9
James Lafferty <james@j-lafferty.net>
2020-08-14 17:59:30 -0700

    Change where #round(2) is called)

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_tip_calculator.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_tip_calculator.rb
@@ -1,18 +1,17 @@
 # Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip and then display both the tip and the total amount of the bill.
 
 def caltulate_tip(bill, tip_percentage)
-  (bill * (tip_percentage / 100)).round(2)
+  bill * (tip_percentage / 100)
 end
 
 puts "What is the bill?"
-bill = gets.chomp.to_f.round(2)
+bill = gets.chomp.to_f
 
 puts "What is the tip percentage?"
-tip_percentage = gets.chomp.to_f.round(2)
+tip_percentage = gets.chomp.to_f
 
-tip_amount = caltulate_tip(bill, tip_percentage)
-
-total = bill + tip_amount
+tip_amount = caltulate_tip(bill, tip_percentage).round(2)
+total = (bill + tip_amount).round(2)
 
 puts "The tip is $#{tip_amount}"
 puts "The total is $#{total}"
