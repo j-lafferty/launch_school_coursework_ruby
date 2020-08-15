443aa0d6fcbdda5686ceb9d46b3f916e8f46286a
James Lafferty <james@j-lafferty.net>
2020-08-15 11:42:12 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_greeting_a_user.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_greeting_a_user.rb
@@ -1,6 +1,21 @@
 # Write a program that will ask for user's name. The program will then greet the user. If the user writes "name!" then the computer yells back to the user.
 
+def msg(str)
+  puts "=> #{str}"
+end
 
+def greet(name)
+  standard_greet = "Hello #{name}."
+  yell_greet = "HELLO #{name.chop.upcase}. "\
+               "WHY ARE WE SCREAMING?"
+
+  name.end_with?("!") ? msg(yell_greet) : msg(standard_greet)
+end
+
+msg("What is your name?")
+name = gets.chomp
+
+greet(name)
 
 # Examples
 
