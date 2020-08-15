68626e9e8d10f426b7ef6190c2f3120578a028ae
James Lafferty <james@j-lafferty.net>
2020-08-15 11:04:06 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_when_will_i_retire.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_when_will_i_retire.rb
@@ -1,6 +1,36 @@
 # Build a program that displays when the user will retire and how many years she has to work till retirement.
 
+require 'date'
 
+CURRENT_YEAR = Date.today.year
+
+def msg(str)
+  puts "=> #{str}"
+end
+
+def years_to_work(age, age_to_retire)
+  age_to_retire - age
+end
+
+def retirement_year(current_year, age_to_retire)
+  current_year + age_to_retire
+end
+
+def display(age, age_to_retire, current_year)
+  working_years = years_to_work(age, age_to_retire)
+  year_of_retirement = retirement_year(current_year, age_to_retire)
+
+  msg("It's #{current_year}. You will retire in #{year_of_retirement}.")
+  msg("You have only #{working_years} years of work to go!")
+end
+
+msg("What is your age?")
+age = gets.chomp.to_i
+
+msg("At what age would you like to retire?")
+age_to_retire = gets.chomp.to_i
+
+display(age, age_to_retire, CURRENT_YEAR)
 
 # Example:
 
