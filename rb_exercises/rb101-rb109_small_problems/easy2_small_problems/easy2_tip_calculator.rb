11848b17b76d221ceda4b7bc424deb1b155a8479
James Lafferty <james@j-lafferty.net>
2020-08-14 17:45:30 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_tip_calculator.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_tip_calculator.rb
@@ -1,6 +1,21 @@
 # Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip and then display both the tip and the total amount of the bill.
 
+def caltulate_tip(bill, tip_percentage)
+  (bill * (tip_percentage / 100)).round(2)
+end
 
+puts "What is the bill?"
+bill = gets.chomp.to_f.round(2)
+
+puts "What is the tip percentage?"
+tip_percentage = gets.chomp.to_f.round(2)
+
+tip_amount = caltulate_tip(bill, tip_percentage)
+
+total = bill + tip_amount
+
+puts "The tip is $#{tip_amount}"
+puts "The total is $#{total}"
 
 # Example:
 # What is the bill? 200
