819d499a1ba1f55b389210fe13b28d44b9b57ce5
James Lafferty <james@j-lafferty.net>
2020-08-15 14:18:15 -0700

    Refactor for Rubyish way of coding

--- rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_sum_or_product_of_consecutive_integers.rb
+++ rb_exercises/rb101-rb109_small_problems/easy2_small_problems/easy2_sum_or_product_of_consecutive_integers.rb
@@ -1,6 +1,40 @@
 # Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to determine the sum or product of all numbers between 1 and the entered integer.
 
+def msg(str)
+  puts "=> #{str}"
+end
 
+def sum(number)
+  (1..number).reduce(:+) # (:+) same as { |sum, i| sum += i }
+end
+
+def product(number)
+  (1..number).reduce(:*) # (:*) same as { |product, i| product *= i }
+end
+
+def calculate(input, number)
+  case input
+  when 's' then sum(number)
+  when 'p' then product(number)
+  end
+end
+
+def display(input, number, result)
+  case input
+  when 's' then msg("The sum of the integers between 1 and #{number} is #{result}.")
+  when 'p' then msg("The product of the integers between 1 and #{number} is #{result}.")
+  end
+end
+
+msg("Please enter an integer greater than 0: ")
+number = gets.chomp.to_i
+
+msg("Enter 's' to compute the sum, 'p' to compute the product.")
+input = gets.chomp.downcase
+
+result = calculate(input, number)
+
+display(input, number, result)
 
 
 # Examples:
