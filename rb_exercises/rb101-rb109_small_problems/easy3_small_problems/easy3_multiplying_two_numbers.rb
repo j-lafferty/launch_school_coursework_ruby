666f47cd606f87b7ae0f7583641448be74f037be
James Lafferty <james@j-lafferty.net>
2020-08-18 11:34:14 -0700

    Add integer validation

--- rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_multiplying_two_numbers.rb
+++ rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_multiplying_two_numbers.rb
@@ -5,20 +5,41 @@ def msg(str)
 end
 
 def multiply(num1, num2)
-  num1 * num2
+  [num1.to_i, num2.to_i].reduce(:*)
 end
 
 def display(num1, num2)
   msg("#{num1} * #{num2} = #{multiply(num1, num2)}")
 end
 
-msg("Enter the first number: ")
-number1 = gets.chomp.to_i
+loop do
+  number1 = nil
+  number2 = nil
+  loop do
+    msg("Enter the first number: ")
+    number1 = gets.chomp
+    
+    break if number1.to_i.to_s == number1
+    msg("Invalid input, try again.")
+  end
+  loop do
+    msg("Enter the second number: ")
+    number2 = gets.chomp
+    
+    break if number2.to_i.to_s == number2
+    msg("Invalid input, try again.")
+  end
 
-msg("Enter the second number: ")
-number2 = gets.chomp.to_i
+  display(number1, number2)
 
-display(number1, number2)
+  msg("")
+  msg("Do you want to multiply other numbers? (y or n)")
+  input = gets.chomp.downcase
+
+  break if input == 'n'
+end
+
+msg("Goodbye!")
 
 # Example:
 
