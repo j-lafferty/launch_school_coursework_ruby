918b6f910fa6874e81bc5e0fcfbdacfa49bbd817
James Lafferty <james@j-lafferty.net>
2020-08-18 15:53:03 -0700

    Add alternate solution for selecting odds; add solution to select evens

--- rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_odd_lists.rb
+++ rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_odd_lists.rb
@@ -5,6 +5,21 @@ def oddities(arg)
   arg.reject { |i| arg.index(i).odd? }
 end
 
+# return array of even index items (or odd elements)
+def oddities2(arg)
+  arg.select { |i| arg.index(i).even? }
+end
+
+# return array of odd index items (or even elements)
+def evens(arg)
+  arg.reject { |i| arg.index(i).even? }
+end
+
+# return array of odd index items (or even elements)
+def evens2(arg)
+  arg.select { |i| arg.index(i).odd? }
+end
+
 # Examples:
 # all puts true
 puts oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
@@ -12,3 +27,27 @@ puts oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
 puts oddities(['abc', 'def']) == ['abc']
 puts oddities([123]) == [123]
 puts oddities([]) == []
+
+# Examples:
+# all puts true
+puts oddities2([2, 3, 4, 5, 6]) == [2, 4, 6]
+puts oddities2([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+puts oddities2(['abc', 'def']) == ['abc']
+puts oddities2([123]) == [123]
+puts oddities2([]) == []
+
+# Examples:
+# all puts true
+puts evens([2, 3, 4, 5, 6]) == [3, 5]
+puts evens([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
+puts evens(['abc', 'def']) == ['def']
+puts evens([123]) == []
+puts evens([]) == []
+
+# Examples:
+# all puts true
+puts evens2([2, 3, 4, 5, 6]) == [3, 5]
+puts evens2([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
+puts evens2(['abc', 'def']) == ['def']
+puts evens2([123]) == []
+puts evens2([]) == []
