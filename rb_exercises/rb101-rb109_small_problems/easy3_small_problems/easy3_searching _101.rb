ba9901afc6ab4a84605cfae030517c63b43a0fa8
James Lafferty <james@j-lafferty.net>
2020-08-17 18:07:03 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_searching _101.rb
+++ rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_searching _101.rb
@@ -1,8 +1,47 @@
 # Write a program that solicits 6 numbers from the user, then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
 
+user_numbers = []
+
 def msg(str)
-  puts "=> #{msg}"
+  puts "=> #{str}"
 end
+
+def input_check(user_numbers)
+  if user_numbers.first(user_numbers.length - 1)
+                 .include?(user_numbers.last)
+    rejected = user_numbers.pop
+    reject_input(user_numbers, rejected)
+  else
+    accept_input(user_numbers)
+  end
+end
+
+def number_suffix(num)
+  case num
+  when 1 then "#{num}st"
+  when 2 then "#{num}nd"
+  when 3 then "#{num}rd"
+  when 4..5 then "#{num}th"
+  when 6 then "last"
+  end
+end
+
+def ask(num, user_numbers)
+  puts "Enter the #{number_suffix(num)} number :"
+  input = gets.chomp.to_i
+  user_numbers << input
+  input_check(user_numbers) if num == 6
+end
+
+def reject_input(user_numbers, rejected)
+  msg("The number #{rejected} appears in #{user_numbers}.")
+end
+
+def accept_input(user_numbers)
+  msg("The number #{user_numbers.last} does not appear in #{user_numbers}.")
+end
+
+1.upto(6) { |i| ask(i, user_numbers)}
 
 
 
