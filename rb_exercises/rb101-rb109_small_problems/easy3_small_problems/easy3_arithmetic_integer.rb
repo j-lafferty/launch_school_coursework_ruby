05d3cd894d2cdebf3fc592bcc876f4e10a54fc44
James Lafferty <james@j-lafferty.net>
2020-08-18 10:25:03 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_arithmetic_integer.rb
+++ rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_arithmetic_integer.rb
@@ -1,8 +1,29 @@
 # Write a program that prompts the user for two positive integers, and then prints the results of the following operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry about validating the input.
 
-def msg(str)
+# constant array of symbols
+OPERATORS = %i(+ - * / % **) # see ruby-doc/syntax/literals/percent-strings
 
+def msg(str)
+  puts "=> #{str}"
 end
+
+def arithmetic(num1, num2, sym)
+  [num1, num2].reduce(sym)
+end
+
+def display(num1, num2, op)
+  op.each do |sym|
+    msg("#{num1} #{sym} #{num2} = #{arithmetic(num1, num2, sym)}")
+  end
+end
+
+puts "Enter the first number: "
+number1 = gets.chomp.to_i
+
+puts "Enter the second number: "
+number2 = gets.chomp.to_i
+
+display(number1, number2, OPERATORS)
 
 # Example
 
