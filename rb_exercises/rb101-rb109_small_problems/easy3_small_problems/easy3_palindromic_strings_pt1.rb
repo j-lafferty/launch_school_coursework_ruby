02f70a3186c046a537441b1bd37151acd91200bb
James Lafferty <james@j-lafferty.net>
2020-08-19 15:53:38 -0700

    Add solution for array argument

--- rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_palindromic_strings_pt1.rb
+++ rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_palindromic_strings_pt1.rb
@@ -4,9 +4,19 @@ def palindrome?(str)
   str == str.reverse
 end
 
-# Examples:
+def arr_palindrome?(arr)
+  arr == arr.reverse
+end
 
+# Examples:
 puts palindrome?('Madam') == false          # (case matters)
 puts palindrome?("madam i'm adam") == false # (all characters matter)
 puts palindrome?('madam') == true
 puts palindrome?('356653') == true
+
+# Examples:
+puts arr_palindrome?(%w(Madam Sir cat dog)) == false          
+puts arr_palindrome?(%w(Madam cat dog cat madam)) == false # (case matters)
+puts arr_palindrome?(%w(Madam Sir cat dog cat Sir Madam)) == true
+puts arr_palindrome?([1, 2, 3, 4, 3, 2, 1]) == true
+puts arr_palindrome?([1]) == true
