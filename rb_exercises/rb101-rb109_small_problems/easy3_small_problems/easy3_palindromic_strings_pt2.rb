633bbe771ac47fcbe978c54e25fc6b9ddff0748f
James Lafferty <james@j-lafferty.net>
2020-08-19 16:31:58 -0700

    Update to call palindrome? method

--- rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_palindromic_strings_pt2.rb
+++ rb_exercises/rb101-rb109_small_problems/easy3_small_problems/easy3_palindromic_strings_pt2.rb
@@ -1,8 +1,12 @@
 # Write another method that returns true if the string passed as an argument is a palindrome, false otherwise. This time, however, your method should be case-insensitive, and it should ignore all non-alphanumeric characters. If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
 
+def palindrome?(str)
+  str == str.reverse
+end
+
 def real_palindrome?(arg)
   formatted_arg = arg.downcase.gsub(/[^a-z0-9]/, '')
-  formatted_arg == formatted_arg.reverse
+  palindrome?(formatted_arg)
 end
 
 # Examples:
