22211845dad5e551f2ed6cfe3a09da4752e50e22
James Lafferty <james@j-lafferty.net>
2020-08-20 19:11:16 -0700

    Remove unneeded else statement

--- rb_exercises/rb101-rb109_small_problems/easy4_small_problems/easy4_leap_year_pt1.rb
+++ rb_exercises/rb101-rb109_small_problems/easy4_small_problems/easy4_leap_year_pt1.rb
@@ -4,11 +4,8 @@
 
 def leap_year?(year)
   if year > 0
-    if ( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0
-      true
-    else
-      false
-    end
+    return true if ( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0
+    false
   end
 end
 
