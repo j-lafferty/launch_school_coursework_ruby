da0c8af5f929ae8ad58206dc5af38cd70813b03e
James Lafferty <james@j-lafferty.net>
2020-08-21 13:01:41 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy4_small_problems/easy4_multiples_of_3_and_5.rb
@@ -0,0 +1,19 @@
+# Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number, and then computes the sum of those multiples. For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+# You may assume that the number passed in is an integer greater than 1.
+
+def multiple?(num, divisor)
+  num % divisor == 0
+end
+
+def multisum(num)
+  sum = 0
+  1.upto(num) { |i| sum += i if multiple?(i, 3) || multiple?(i, 5) }
+  sum
+end
+
+# Examples
+puts multisum(3) == 3
+puts multisum(5) == 8
+puts multisum(10) == 33
+puts multisum(1000) == 234168
