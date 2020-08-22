e91e1cf39dbe5b098c50b015fd810214c101e983
James Lafferty <james@j-lafferty.net>
2020-08-21 17:27:57 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy4_small_problems/easy4_running_totals.rb
@@ -0,0 +1,12 @@
+# Write a method that takes an Array of numbers, and returns an Array with the same number of elements, and each element has the running total from the original Array.
+
+def running_total(arr)
+  sum = 0
+  arr.map {|i| sum += i}
+end
+
+# Examples:
+puts running_total([2, 5, 13]) == [2, 7, 20]
+puts running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+puts running_total([3]) == [3]
+puts running_total([]) == []
