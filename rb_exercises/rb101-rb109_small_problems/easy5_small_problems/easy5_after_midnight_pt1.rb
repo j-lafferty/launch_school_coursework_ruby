e9fa5c3a7330dc7b5d5cc887c99b109a6e1212d0
James Lafferty <james@j-lafferty.net>
2020-08-24 12:28:53 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_after_midnight_pt1.rb
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_after_midnight_pt1.rb
@@ -4,16 +4,22 @@
 
 # You may not use ruby's Date and Time classes.
 
-def time_of_day(num)
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
+def time_of_day(minutes)
+  input_minutes = minutes % MINUTES_PER_DAY
+  hr, min = input_minutes.divmod(MINUTES_PER_HOUR)
+  sprintf('%02d:%02d', hr, min)
 end
 
 # Examples:
-time_of_day(0) == "00:00"
-time_of_day(-3) == "23:57"
-time_of_day(35) == "00:35"
-time_of_day(-1437) == "00:03"
-time_of_day(3000) == "02:00"
-time_of_day(800) == "13:20"
-time_of_day(-4231) == "01:29"
+puts time_of_day(0) == "00:00"
+puts time_of_day(-3) == "23:57"
+puts time_of_day(35) == "00:35"
+puts time_of_day(-1437) == "00:03"
+puts time_of_day(3000) == "02:00"
+puts time_of_day(800) == "13:20"
+puts time_of_day(-4231) == "01:29"
 # Disregard Daylight Savings and Standard Time and other complications.
