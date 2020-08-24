4d8c15fdaadc7651bc468e48c1cb2536c94bbfb9
James Lafferty <james@j-lafferty.net>
2020-08-24 10:17:46 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_acsii_string_value.rb
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_acsii_string_value.rb
@@ -1,10 +1,10 @@
 # Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. The ASCII string value is the sum of the ASCII values of every character in the string. (You may use String#ord to determine the ASCII value of a character.)
 
-def ascii_value
-
+def ascii_value(str)
+  str.chars.inject(0) { |sum, i| sum + i.ord }
 end
 
-ascii_value('Four score') == 984
-ascii_value('Launch School') == 1251
-ascii_value('a') == 97
-ascii_value('') == 0
+puts ascii_value('Four score') == 984
+puts ascii_value('Launch School') == 1251
+puts ascii_value('a') == 97
+puts ascii_value('') == 0
