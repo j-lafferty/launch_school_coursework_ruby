debc3a183ebd36e5f79683fca8ae579fcf7b9d90
James Lafferty <james@j-lafferty.net>
2020-08-24 16:26:27 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_after_midnight_pt2.rb
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_after_midnight_pt2.rb
@@ -4,18 +4,36 @@
 
 # You may not use ruby's Date and Time methods.
 
-def after_midnight(time)
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
+def get_hours(time)
+  time.split(//).take(2).join.to_i
+end
+
+def get_minutes(time)
+  time.split(//).drop(3).join.to_i
+end
+
+def after_midnight(time)
+  hr = get_hours(time)
+  min = get_minutes(time)
+
+  minutes = (hr * MINUTES_PER_HOUR) + min
+  minutes %= MINUTES_PER_DAY
 end
 
 def before_midnight(time)
-
+  minutes = MINUTES_PER_DAY - after_midnight(time)
+  minutes = 0 if minutes == MINUTES_PER_DAY
+  minutes
 end
 
 # Examples:
-after_midnight('00:00') == 0
-before_midnight('00:00') == 0
-after_midnight('12:34') == 754
-before_midnight('12:34') == 686
-after_midnight('24:00') == 0
-before_midnight('24:00') == 0
+puts after_midnight('00:00') == 0
+puts before_midnight('00:00') == 0
+puts after_midnight('12:34') == 754
+puts before_midnight('12:34') == 686
+puts after_midnight('24:00') == 0
+puts before_midnight('24:00') == 0
