5fcf8a9bf80a48ea3e714f39049bd6646c7e59ca
James Lafferty <james@j-lafferty.net>
2020-08-25 16:16:31 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_letter_counter_pt1.rb
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_letter_counter_pt1.rb
@@ -3,11 +3,16 @@
 # Words consist of any string of characters that do not include a space.
 
 def word_sizes(str)
+  count = Hash.new(0)
+  str.split.each do |word|
+    count[word.size] += 1
+  end
 
+  count
 end
 
 # Examples
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
-word_sizes('') == {}
+puts word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+puts word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+puts word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+puts word_sizes('') == {}
