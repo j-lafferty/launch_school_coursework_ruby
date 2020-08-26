30f354eb3174367a793e64ae8ff820dd2d989a19
James Lafferty <james@j-lafferty.net>
2020-08-26 16:41:27 -0700

    Add explanation as solution

--- rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_spin_me_around_in_circles.rb
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_spin_me_around_in_circles.rb
@@ -1,5 +1,7 @@
 # You are given a method named spin_me that takes a string as an argument and returns a string that contains the same words, but with each word's characters reversed. Given the method's implementation, will the returned string be the same object as the one passed in as an argument or a different object?
 
+# returns a different string object.
+# String#split converts the string object into an Array object
 def spin_me(str)
   str.split.each do |word|
     word.reverse!
@@ -7,3 +9,7 @@ def spin_me(str)
 end
 
 spin_me("hello world") # "olleh dlrow"
+
+str = 'hello world'
+puts str.object_id # 47435609148580
+puts spin_me(str).object_id # 47435609148360
