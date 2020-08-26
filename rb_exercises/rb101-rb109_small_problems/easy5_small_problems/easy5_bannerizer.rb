35eacd9d5d3931fa3067f374bf21f383d6571746
James Lafferty <james@j-lafferty.net>
2020-08-26 15:34:09 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_bannerizer.rb
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_bannerizer.rb
@@ -1,7 +1,23 @@
 # Write a method that will take a short line of text, and print it within a box.
 
-def print_in_box(str)
+def box_dash(str)
+  dashes = ''
+  str.size.times { |i| dashes << '-' }
+  dashes
+end
 
+def box_space(str)
+  spaces = ''
+  str.size.times { |i| spaces << ' ' }
+  spaces
+end
+
+def print_in_box(str)
+  puts "+-#{box_dash(str)}-+"
+  puts "| #{box_space(str)} |"
+  puts "| #{str} |"
+  puts "| #{box_space(str)} |"
+  puts "+-#{box_dash(str)}-+"
 end
 
 # Example:
@@ -12,7 +28,8 @@ print_in_box('To boldly go where no one has gone before.')
 # | To boldly go where no one has gone before. |
 # |                                            |
 # +--------------------------------------------+
-# print_in_box('')
+
+print_in_box('')
 # +--+
 # |  |
 # |  |
