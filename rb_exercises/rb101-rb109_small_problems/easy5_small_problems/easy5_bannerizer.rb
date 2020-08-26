cace7a29f3b4fd8bdd6fa91cf05ea70fe24b868e
James Lafferty <james@j-lafferty.net>
2020-08-26 15:12:03 -0700

    Initial commit

---
+++ rb_exercises/rb101-rb109_small_problems/easy5_small_problems/easy5_bannerizer.rb
@@ -0,0 +1,21 @@
+# Write a method that will take a short line of text, and print it within a box.
+
+def print_in_box(str)
+
+end
+
+# Example:
+
+print_in_box('To boldly go where no one has gone before.')
+# +--------------------------------------------+
+# |                                            |
+# | To boldly go where no one has gone before. |
+# |                                            |
+# +--------------------------------------------+
+# print_in_box('')
+# +--+
+# |  |
+# |  |
+# |  |
+# +--+
+# You may assume that the input will always fit in your terminal window.
