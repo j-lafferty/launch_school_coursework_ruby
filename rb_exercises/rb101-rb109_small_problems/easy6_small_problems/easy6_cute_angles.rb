d7d7cd244ccb0e048726581cf96a10cbb45910c6
James Lafferty <james@j-lafferty.net>
2020-08-27 13:18:06 -0700

    Add more accurate solution

--- rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_cute_angles.rb
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_cute_angles.rb
@@ -1,18 +1,33 @@
 # Write a method that takes a floating point number that represents an angle between 0 and 360 degrees and returns a String that represents that angle in degrees, minutes and seconds. You should use a degree symbol (°) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. A degree has 60 minutes, while a minute has 60 seconds.
 
 DEGREE = "\xC2\xB0"
+MINUTES_PER_SECOND = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_SECOND * SECONDS_PER_MINUTE
 
 def display(deg, min, sec)
   puts format(%(#{deg}#{DEGREE}%02d'%02d"), min, sec)
 end
 
-def dms(angle)
-  degrees, d_remainder = angle.divmod(1)
-  minutes, m_remainder = (d_remainder * 60).divmod(1)
-  seconds = (m_remainder * 60)
+# some calculations are off by 1 second; e.g. dms(76.73)
+# working with total seconds would be more accurate
+# def dms(angle)
+#   degrees, d_remainder = angle.divmod(1)
+#   minutes, m_remainder = (d_remainder * 60).divmod(1)
+#   seconds = (m_remainder * 60)
 
+#   display(degrees, minutes, seconds)
+# end
+
+# more accurate solution; working with total seconds
+def dms(angle)
+  total_seconds = (angle * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  
   display(degrees, minutes, seconds)
 end
+
 
 # Examples:
 
