7a4bd9b54d8fac8b4f35a913364861f981076b9b
James Lafferty <james@j-lafferty.net>
2020-08-28 16:16:26 -0700

    Add comment to solution

--- rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_halvsies.rb
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_halvsies.rb
@@ -1,6 +1,7 @@
 # Write a method that takes an Array as an argument, and returns two Arrays (as a pair of nested Arrays) that contain the first half and second half of the original Array, respectively. If the original array contains an odd number of elements, the middle element should be placed in the first half Array.
 
 def halvsies(arr)
+  # divide arr.size by 2.0 so #ceil will round up when #size is odd
   arr.partition { |v| arr.index(v) < (arr.size / 2.0).ceil }
 end
 
