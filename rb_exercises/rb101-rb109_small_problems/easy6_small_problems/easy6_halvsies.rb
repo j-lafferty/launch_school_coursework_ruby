f96ccc0d5f573c6f38b38772e94cf5816a72e34d
James Lafferty <james@j-lafferty.net>
2020-08-28 16:13:38 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_halvsies.rb
@@ -0,0 +1,12 @@
+# Write a method that takes an Array as an argument, and returns two Arrays (as a pair of nested Arrays) that contain the first half and second half of the original Array, respectively. If the original array contains an odd number of elements, the middle element should be placed in the first half Array.
+
+def halvsies(arr)
+  arr.partition { |v| arr.index(v) < (arr.size / 2.0).ceil }
+end
+
+# Examples:
+
+puts halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+puts halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+puts halvsies([5]) == [[5], []]
+puts halvsies([]) == [[], []]
