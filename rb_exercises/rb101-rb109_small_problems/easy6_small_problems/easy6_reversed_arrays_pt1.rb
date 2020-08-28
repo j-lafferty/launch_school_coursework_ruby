bffd9dc174ee22535a0ba7b680cb3583291ad9dd
James Lafferty <james@j-lafferty.net>
2020-08-28 09:30:16 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_reversed_arrays_pt1.rb
@@ -0,0 +1,35 @@
+# Write a method that takes an Array as an argument, and reverses its elements in place; that is, mutate the Array passed into this method. The return value should be the same Array object.
+
+# You may not use Array#reverse or Array#reverse!.
+
+def reverse!(arr)
+  reverse_arr = []
+  i = 0
+  
+  while i < arr.size
+    reverse_arr.unshift(arr[i])
+    i += 1
+  end
+  
+  arr.replace(reverse_arr)
+end
+
+# Examples:
+
+list = [1,2,3,4]
+result = reverse!(list)
+puts result == [4, 3, 2, 1]
+puts list == [4, 3, 2, 1]
+puts list.object_id == result.object_id
+
+list = %w(a b e d c)
+puts reverse!(list) == ["c", "d", "e", "b", "a"]
+puts list == ["c", "d", "e", "b", "a"]
+
+list = ['abc']
+puts reverse!(list) == ["abc"]
+puts list == ["abc"]
+
+list = []
+puts reverse!(list) == []
+puts list == []
