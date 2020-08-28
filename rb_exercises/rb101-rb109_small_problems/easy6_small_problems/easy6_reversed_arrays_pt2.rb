134be98ebc2d55e0c2051858315ca59f55010b2b
James Lafferty <james@j-lafferty.net>
2020-08-28 10:58:07 -0700

    Add comment to solution

--- rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_reversed_arrays_pt2.rb
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_reversed_arrays_pt2.rb
@@ -2,6 +2,7 @@
 
 # You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
 
+# similar solution to previous problem, but use #map to return new array
 def reverse(arr)
   reverse_arr = []
   arr.map { |i| reverse_arr.unshift(i) }.replace(reverse_arr)
