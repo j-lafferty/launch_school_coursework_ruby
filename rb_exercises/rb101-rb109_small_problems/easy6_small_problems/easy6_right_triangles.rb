ae90e4ed8a55857f08a62621413753dabfb49bda
James Lafferty <james@j-lafferty.net>
2020-08-29 11:00:32 -0700

    Add solution for any size and point location

--- rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_right_triangles.rb
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_right_triangles.rb
@@ -1,23 +1,48 @@
 # Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
 
-def triangle(num)
-  1.upto(num) { |i| puts ('*' * i).rjust(num) }
+# further exploration: any corner, any size
+# does not have input validation
+def triangle(num, just)
+  case just
+  when 'left' then 1.upto(num) { |i| puts ('*' * i).ljust(num) }
+  when 'right' then 1.upto(num) { |i| puts ('*' * i).rjust(num) }
+  end
 end
 
-def upside_down_triangle(num)
-  num.downto(1) { |i| puts ('*' * i).rjust(num) }
+def upside_down_triangle(num, just)
+  case just
+  when 'left' then num.downto(1) { |i| puts ('*' * i).ljust(num) }
+  when 'right' then num.downto(1) { |i| puts ('*' * i).rjust(num) }
+  end
+end
+
+def triangle_shape?
+  puts "Enter height of triangle: "
+  num = gets.chomp.to_i
+  
+  puts "Select corner for triangle point: top-l, top-r, bottom-l, bottom-r"
+  corner = gets.chomp
+
+  case corner
+  when 'top-l' then triangle(num, 'left')
+  when 'top-r' then triangle(num, 'right')
+  when 'bottom-l' then upside_down_triangle(num, 'left')
+  when 'bottom-r' then upside_down_triangle(num, 'right')
+  end
 end
 
 # Examples:
 
-triangle(5)
+triangle_shape?
+
+# triangle(5)
 
 # #     *
 # #    **
 # #   ***
 # #  ****
 # # *****
-triangle(9)
+# triangle(9)
 
 # #         *
 # #        **
@@ -29,5 +54,5 @@ triangle(9)
 # #  ********
 # # *********
 
-upside_down_triangle(5)
-upside_down_triangle(9)
+# upside_down_triangle(5)
+# upside_down_triangle(9)
