a491193ed49116d068354ff452529a3a6f979486
James Lafferty <james@j-lafferty.net>
2020-08-29 08:52:14 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_find_the_duplicate.rb
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_find_the_duplicate.rb
@@ -1,13 +1,13 @@
 # Given an unordered array and the information that exactly one value in the array occurs twice (every other value occurs exactly once), how would you determine which value occurs twice? Write a method that will find and return the duplicate value that is known to be in the array.
 
 def find_dup(arr)
-
+  arr.sum - arr.uniq.sum
 end
 
 # Examples:
 
-find_dup([1, 5, 3, 1]) == 1
-find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+puts find_dup([1, 5, 3, 1]) == 1
+puts find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
           38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
           14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
           78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
