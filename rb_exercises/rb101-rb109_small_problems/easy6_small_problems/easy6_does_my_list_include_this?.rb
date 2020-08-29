ec46c648c0d74245c0ffcdaae8e665e90fc1d217
James Lafferty <james@j-lafferty.net>
2020-08-29 09:40:35 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy6_small_problems/easy6_does_my_list_include_this?.rb
@@ -0,0 +1,12 @@
+# Write a method named include? that takes an Array and a search value as arguments. This method should return true if the search value is in the array, false if it is not. You may not use the Array#include? method in your solution.
+
+# could have used #any?, but that seemed too similar to #include?
+def include?(arr, num)
+  arr.select { |i| i == num }.size > 0
+end
+
+puts include?([1,2,3,4,5], 3) == true
+puts include?([1,2,3,4,5], 6) == false
+puts include?([], 3) == false
+puts include?([nil], nil) == true
+puts include?([], nil) == false
