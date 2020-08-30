64f40daaaa338ab47b43968050f7d399beb12dca
James Lafferty <james@j-lafferty.net>
2020-08-30 09:14:17 -0700

    Remove #downcase call on str

--- rb_exercises/rb101-rb109_small_problems/easy7_small_problems/easy7_staggered_caps_pt1.rb
+++ rb_exercises/rb101-rb109_small_problems/easy7_small_problems/easy7_staggered_caps_pt1.rb
@@ -1,7 +1,7 @@
 # Write a method that takes a String as an argument, and returns a new String that contains the original value using a staggered capitalization scheme in which every other character is capitalized, and the remaining characters are lowercase. Characters that are not letters should not be changed, but count as characters when switching between upper and lowercase.
 
 def staggered_case(str)
-  str.downcase.chars.each_with_index.map { |c, idx| idx.even? ? c.upcase : c.downcase }.join
+  str.chars.each_with_index.map { |c, idx| idx.even? ? c.upcase : c.downcase }.join
 end
 
 # Example:
