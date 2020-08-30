b42496750de8f0773f1d26e04b8da8f514596d1c
James Lafferty <james@j-lafferty.net>
2020-08-30 09:12:41 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy7_small_problems/easy7_staggered_caps_pt1.rb
@@ -0,0 +1,11 @@
+# Write a method that takes a String as an argument, and returns a new String that contains the original value using a staggered capitalization scheme in which every other character is capitalized, and the remaining characters are lowercase. Characters that are not letters should not be changed, but count as characters when switching between upper and lowercase.
+
+def staggered_case(str)
+  str.downcase.chars.each_with_index.map { |c, idx| idx.even? ? c.upcase : c.downcase }.join
+end
+
+# Example:
+
+puts staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+puts staggered_case('ALL_CAPS') == 'AlL_CaPs'
+puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
