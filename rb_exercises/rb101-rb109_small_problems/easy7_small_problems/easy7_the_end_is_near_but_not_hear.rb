2caf16d6a28057ab8fc3ca6b10c670445eed04d9
James Lafferty <james@j-lafferty.net>
2020-08-30 15:01:07 -0700

    Add solution for additional edge cases

--- rb_exercises/rb101-rb109_small_problems/easy7_small_problems/easy7_the_end_is_near_but_not_hear.rb
+++ rb_exercises/rb101-rb109_small_problems/easy7_small_problems/easy7_the_end_is_near_but_not_hear.rb
@@ -8,7 +8,24 @@ def penultimate(str)
   str.split[-2]
 end
 
+# Further Exploration
+# Our solution ignored a couple of edge cases because we explicitly stated that you didn't have to handle them: strings that contain just one word, and strings that contain no words.
+
+# Suppose we need a method that retrieves the middle word of a phrase/sentence. What edge cases need to be considered? How would you handle those edge cases without ignoring them? Write a method that returns the middle word of a phrase or sentence. It should handle all of the edge cases you thought of.
+
+def penultimate_further(str)
+  arr = str.split
+  case
+  when arr.size <= 1 then arr[0]
+  else arr.at((arr.size / 2.0).ceil - 1)
+  end
+end
+
 # Examples:
 
 puts penultimate('last word') == 'last'
 puts penultimate('Launch School is great!') == 'is'
+
+puts penultimate_further('Launch School is great!') == 'School'
+puts penultimate_further('Hello') == 'Hello'
+puts penultimate_further('Suppose we need a method') == 'need'
