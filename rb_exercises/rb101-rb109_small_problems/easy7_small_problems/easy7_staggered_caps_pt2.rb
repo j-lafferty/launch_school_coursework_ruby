d3f7335fb0f6cd2e2d4e87a83500bd425ac08545
James Lafferty <james@j-lafferty.net>
2020-08-30 10:33:29 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy7_small_problems/easy7_staggered_caps_pt2.rb
@@ -0,0 +1,24 @@
+# Modify the method from the previous exercise so it ignores non-alphabetic characters when determining whether it should uppercase or lowercase each letter. The non-alphabetic characters should still be included in the return value; they just don't count when toggling the desired case.
+
+def staggered_case(str)
+  # select non-alphabetic characters with their index
+  non_alpha = str.chars.each_with_index.select { |c, idx| idx if c.match?(/[^a-z]/i) }
+
+  # strip all non-alphabetic characters
+  only_aplha = str.chars.select { |c| c if c.match?(/[a-z]/i) }
+
+  # stagger case
+  staggered = only_aplha.each_with_index.map { |c, idx| idx.even? ? c.upcase : c.downcase }
+
+  #insert non-alphabetic character into original index location
+  non_alpha.each do |i|
+    staggered.insert(i[1], i[0])
+  end
+  staggered.join
+end
+
+# Example:
+
+puts staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+puts staggered_case('ALL CAPS') == 'AlL cApS'
+puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
