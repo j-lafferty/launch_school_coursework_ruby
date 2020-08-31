20b364e7b716e56d95ad97187d345358eafabec9
James Lafferty <james@j-lafferty.net>
2020-08-31 16:29:11 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy8_small_problems/easy8_sum_of_sums.rb
+++ rb_exercises/rb101-rb109_small_problems/easy8_small_problems/easy8_sum_of_sums.rb
@@ -1,12 +1,14 @@
 # Write a method that takes an Array of numbers and then returns the sum of the sums of each leading subsequence for that Array. You may assume that the Array always contains at least one number.
 
 def sum_of_sums(arr)
-  
+  sum = 0
+  arr.size.times { |i| sum += arr[0..i].sum }
+  sum
 end
 
 # Examples:
 
-sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
-sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
-sum_of_sums([4]) == 4
-sum_of_sums([1, 2, 3, 4, 5]) == 35
+puts sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+puts sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+puts sum_of_sums([4]) == 4
+puts sum_of_sums([1, 2, 3, 4, 5]) == 35
