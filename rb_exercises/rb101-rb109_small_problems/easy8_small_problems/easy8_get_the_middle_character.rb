86dc87495c1e9d8d5df73bbdce44731ade63d23d
James Lafferty <james@j-lafferty.net>
2020-09-01 16:44:47 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy8_small_problems/easy8_get_the_middle_character.rb
@@ -0,0 +1,14 @@
+# Write a method that takes a non-empty string argument, and returns the middle character or characters of the argument. If the argument has an odd length, you should return exactly one character. If the argument has an even length, you should return exactly two characters.
+
+def center_of(str)
+  str_center = str.size / 2
+  str.size.odd? ? str[str_center] : str[str_center - 1, 2]
+end
+
+# Examples:
+
+puts center_of('I love ruby') == 'e'
+puts center_of('Launch School') == ' '
+puts center_of('Launch') == 'un'
+puts center_of('Launchschool') == 'hs'
+puts center_of('x') == 'x'
