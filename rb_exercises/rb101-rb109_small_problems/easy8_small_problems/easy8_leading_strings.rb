9c1de79c713447a34a5e8bdbec60ce1b547868ef
James Lafferty <james@j-lafferty.net>
2020-08-31 17:31:04 -0700

    Initial commit

---
+++ rb_exercises/rb101-rb109_small_problems/easy8_small_problems/easy8_leading_strings.rb
@@ -0,0 +1,11 @@
+# Write a method that returns a list of all substrings of a string that start at the beginning of the original string. The return value should be arranged in order from shortest to longest substring.
+
+def leading_substrings(str)
+
+end
+
+# Examples:
+
+leading_substrings('abc') == ['a', 'ab', 'abc']
+leading_substrings('a') == ['a']
+leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
