90936b78b484b12257eaf87004e6838510230b05
James Lafferty <james@j-lafferty.net>
2020-09-03 08:13:48 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_double_doubles.rb
+++ rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_double_doubles.rb
@@ -3,23 +3,31 @@
 # Write a method that returns 2 times the number provided as an argument, unless the argument is a double number; double numbers should be returned as-is.
 
 def double?(num)
-
+  digits = num.digits.reverse
+  num_center = digits.size / 2
+  
+  if digits.size.even?
+    if digits[0..num_center - 1] == digits[num_center..-1]
+      num
+    end
+  end
 end
 
 def twice(num)
-  num * 2 unless double?(num)
+  double_check = double?(num)
+  double_check ? double_check : num * 2
 end
 
 # Examples:
 
-twice(37) == 74
-twice(44) == 44
-twice(334433) == 668866
-twice(444) == 888
-twice(107) == 214
-twice(103103) == 103103
-twice(3333) == 3333
-twice(7676) == 7676
-twice(123_456_789_123_456_789) == 123_456_789_123_456_789
-twice(5) == 10
+puts twice(37) == 74
+puts twice(44) == 44
+puts twice(334433) == 668866
+puts twice(444) == 888
+puts twice(107) == 214
+puts twice(103103) == 103103
+puts twice(3333) == 3333
+puts twice(7676) == 7676
+puts twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+puts twice(5) == 10
 # Note: underscores are used for clarity above. Ruby lets you use underscores when writing long numbers; however, it does not print the underscores when printing long numbers. Don't be alarmed if you don't see the underscores when running your tests.
