202d32775fa71c5b9c1251fd4182eb5930123ddf
James Lafferty <james@j-lafferty.net>
2020-09-03 09:29:39 -0700

    Add solution for further exploration

--- rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_counting_up.rb
+++ rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_counting_up.rb
@@ -6,8 +6,21 @@ def sequence(num)
   (1..num).to_a
 end
 
-# Examples:
+# Further Exploration
+# Food for thought: what do you think sequence should return if the argument is not greater than 0. For instance, what should you do if the argument is -1? Can you alter your method to handle this case?
 
+def sequence_further(num)
+  num > 0 ? [*1..num] : [*num..-1]
+end
+
+
+puts "Examples:"
 puts sequence(5) == [1, 2, 3, 4, 5]
 puts sequence(3) == [1, 2, 3]
 puts sequence(1) == [1]
+
+puts "Further Exploration Examples:"
+
+puts sequence_further(-5) == [-5, -4, -3, -2, -1]
+puts sequence_further(3) == [1, 2, 3]
+puts sequence_further(-1) == [-1]
