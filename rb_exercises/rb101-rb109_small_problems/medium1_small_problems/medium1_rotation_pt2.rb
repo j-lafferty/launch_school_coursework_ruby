bc626d0d37f478d1982865827f89acedacaf2653
James Lafferty <james@j-lafferty.net>
2020-09-03 15:26:39 -0700

    Initial commit

---
+++ rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_rotation_pt2.rb
@@ -0,0 +1,18 @@
+# Write a method that can rotate the last n digits of a number.
+
+def rotate_rightmost_digits(num1, num2)
+
+end
+
+# For example:
+puts rotate_rightmost_digits(735291, 1) == 735291
+puts rotate_rightmost_digits(735291, 2) == 735219
+puts rotate_rightmost_digits(735291, 3) == 735912
+puts rotate_rightmost_digits(735291, 4) == 732915
+puts rotate_rightmost_digits(735291, 5) == 752913
+puts rotate_rightmost_digits(735291, 6) == 352917
+# Note that rotating just 1 digit results in the original number being returned.
+
+# You may use the rotate_array method from the previous exercise if you want. (Recommended!)
+
+# You may assume that n is always a positive integer.
