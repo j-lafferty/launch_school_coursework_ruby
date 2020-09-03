0ad34a82209cce0739ff5414538c36987e618997
James Lafferty <james@j-lafferty.net>
2020-09-03 08:37:01 -0700

    Refactor solution

--- rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_always_return_negative.rb
+++ rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_always_return_negative.rb
@@ -1,7 +1,7 @@
 # Write a method that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the number is 0 or negative, return the original number.
 
 def negative(num)
-  num < 1 ? num : -num
+  -(num.abs)
 end
 
 # Examples:
