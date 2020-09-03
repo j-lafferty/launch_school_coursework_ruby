849c80164c6331ddaf95321810714a46fc2f1775
James Lafferty <james@j-lafferty.net>
2020-09-03 10:56:25 -0700

    Add solution

---
+++ rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_sequence_count.rb
@@ -0,0 +1,14 @@
+# Create a method that takes two integers as arguments. The first argument is a count, and the second is the first number of a sequence that your method will create. The method should return an Array that contains the same number of elements as the count argument, while the values of each element will be multiples of the starting number.
+
+# You may assume that the count argument will always have a value of 0 or greater, while the starting number can be any integer value. If the count is 0, an empty list should be returned.
+
+def sequence(num1, num2)
+  num2.step(by: num2).take(num1)
+end
+
+# Examples:
+
+puts sequence(5, 1) == [1, 2, 3, 4, 5]
+puts sequence(4, -7) == [-7, -14, -21, -28]
+puts sequence(3, 0) == [0, 0, 0]
+puts sequence(0, 1000000) == []
