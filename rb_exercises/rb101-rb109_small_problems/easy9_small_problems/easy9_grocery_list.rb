e036de4369a345e27b0478e7af86a9d12e8d80cb
James Lafferty <james@j-lafferty.net>
2020-09-03 12:56:29 -0700

    Refactor solution

--- rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_grocery_list.rb
+++ rb_exercises/rb101-rb109_small_problems/easy9_small_problems/easy9_grocery_list.rb
@@ -1,16 +1,7 @@
 # Write a method which takes a grocery list (array) of fruits with quantities and converts it into an array of the correct number of each fruit.
 
 def buy_fruit(arr)
-  list = []
-
-  arr.each do |i|
-    n = i[1]
-    until n == 0
-      list << i[0]
-      n -= 1
-    end
-  end
-  list.flatten
+  arr.map { |i| [i[0]] * i[1] }.flatten
 end
 
 # Example:
