f2a3909a41b4af592e18e040e62a4e2613dbc80d
James Lafferty <james@j-lafferty.net>
2020-09-04 15:43:39 -0700

    Add method for printing star

--- rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_diamonds.rb
+++ rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_diamonds.rb
@@ -1,12 +1,16 @@
 # Write a method that displays a 4-pointed diamond in an n x n grid, where n is an odd integer that is supplied as an argument to the method. You may assume that the argument will always be an odd integer.
 
+def star(num, i)
+  puts ("*" * i).center(num)
+end
+
 def top(num)
-  puts "*" if num == 1
-  1.step(num - 2, 2) { |i| puts ("*" * i).center(num) }
+  star(num, 1) if num == 1
+  1.step(num - 2, 2) { |i| star(num, i) }
 end
 
 def bottom(num)
-  (num).step(1, -2) { |i| puts ("*" * i).center(num) }
+  (num).step(1, -2) { |i| star(num, i) }
 end
 
 def diamond(num)
