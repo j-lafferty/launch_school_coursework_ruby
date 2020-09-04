05ce62245cb71c92c2f71fd228ab8a13300f62d6
James Lafferty <james@j-lafferty.net>
2020-09-04 15:24:37 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_diamonds.rb
+++ rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_diamonds.rb
@@ -1,7 +1,17 @@
 # Write a method that displays a 4-pointed diamond in an n x n grid, where n is an odd integer that is supplied as an argument to the method. You may assume that the argument will always be an odd integer.
 
-def diamond(num)
+def top(num)
+  puts "*" if num == 1
+  1.step(num - 2, 2) { |i| puts ("*" * i).center(num) }
+end
 
+def bottom(num)
+  (num).step(1, -2) { |i| puts ("*" * i).center(num) }
+end
+
+def diamond(num)
+  top(num)
+  bottom(num) if num > 1
 end
 
 # Examples
