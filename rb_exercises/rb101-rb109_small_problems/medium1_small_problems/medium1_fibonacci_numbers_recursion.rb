6fa2d96d0c84f13ef4acc859aa24c7afbcf42391
James Lafferty <james@j-lafferty.net>
2020-09-06 13:42:00 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_fibonacci_numbers_recursion.rb
+++ rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_fibonacci_numbers_recursion.rb
@@ -21,15 +21,16 @@
 # Write a recursive method that computes the nth Fibonacci number, where nth is an argument to the method.
 
 def fibonacci(num)
-
+  return 1 if num <= 2
+  fibonacci(num - 1) + fibonacci(num - 2)
 end
 
 # Examples:
 
-fibonacci(1) == 1
-fibonacci(2) == 1
-fibonacci(3) == 2
-fibonacci(4) == 3
-fibonacci(5) == 5
-fibonacci(12) == 144
-fibonacci(20) == 6765
+puts fibonacci(1) == 1
+puts fibonacci(2) == 1
+puts fibonacci(3) == 2
+puts fibonacci(4) == 3
+puts fibonacci(5) == 5
+puts fibonacci(12) == 144
+puts fibonacci(20) == 6765
