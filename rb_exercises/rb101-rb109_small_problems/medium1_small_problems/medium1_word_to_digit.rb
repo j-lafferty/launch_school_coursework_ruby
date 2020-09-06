822ca161d20bde19f6eff1e395921d22e3a1afb9
James Lafferty <james@j-lafferty.net>
2020-09-06 11:10:00 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_word_to_digit.rb
+++ rb_exercises/rb101-rb109_small_problems/medium1_small_problems/medium1_word_to_digit.rb
@@ -1,13 +1,24 @@
 # Write a method that takes a sentence string as input, and returns the same string with any sequence of the words 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
 
-ALPHA_DIGITS = %w(zero one two three four five six seve eight nine)
+ALPHA_DIGITS = %w(zero one two three four five six seven eight nine)
 NUM_DIGITS = [*0..9]
-DIGITS = ALPHA_DIGITS.zip(NUM_DIGITS)
+DIGITS = ALPHA_DIGITS.zip(NUM_DIGITS).to_h
 
 def word_to_digit(str)
   input = str.split
+  result = []
+
+  input.each do |i|
+    punct = i.slice!(-1) if i[-1].match?(/[[:punct:]]/)
+    word = DIGITS.has_key?(i) ? DIGITS[i].to_s : i
+
+    word += punct if punct != nil
+    result << word
+  end
+  
+  result.join(' ')
 end
 
 # Example:
 
-word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
