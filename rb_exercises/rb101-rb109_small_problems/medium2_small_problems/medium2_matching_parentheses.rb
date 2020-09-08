5ffd945af23ea86a5d1a33c46c9397e395cdc117
James Lafferty <james@j-lafferty.net>
2020-09-08 10:04:32 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/medium2_small_problems/medium2_matching_parentheses.rb
+++ rb_exercises/rb101-rb109_small_problems/medium2_small_problems/medium2_matching_parentheses.rb
@@ -1,7 +1,20 @@
 # Write a method that takes a string as argument, and returns true if all parentheses in the string are properly balanced, false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
 
 def balanced?(str)
+  open_paren = (0...str.size).find_all { |i| str[i] == '(' }
+  close_paren = (0...str.size).find_all { |i| str[i] == ')' }
 
+  return true if open_paren.size == 0 && close_paren.size == 0
+
+  result = false
+
+  if open_paren.size == close_paren.size 
+    open_paren.each_with_index do |v, i| 
+      v < close_paren[i] ? result = true : result = false
+    end
+  end
+
+  result
 end
 
 # Examples:
