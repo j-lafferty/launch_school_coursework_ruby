0cfe0b41b4054a8a5ba3d396e02e2866bcfd0ada
James Lafferty <james@j-lafferty.net>
2020-09-08 12:07:04 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/medium2_small_problems/medium2_triangle_sides.rb
+++ rb_exercises/rb101-rb109_small_problems/medium2_small_problems/medium2_triangle_sides.rb
@@ -7,8 +7,32 @@
 
 # Write a method that takes the lengths of the 3 sides of a triangle as arguments, and returns a symbol :equilateral, :isosceles, :scalene, or :invalid depending on whether the triangle is equilateral, isosceles, scalene, or invalid.
 
-def triangle(side1, side2, side3)
+def valid_triangle?(sides, max, min)
+  return false if min <= 0
+  return false if (sides.sum - max) < max
+  true
+end
 
+def equilateral?(sides, max)
+  sides.all?(max)
+end
+
+def isosceles_or_scalene?(sides, max, min)
+  max_arr = sides.select { |i| i == max}
+  min_arr = sides.select { |i| i == min}
+
+  max_arr.size == 2 || min_arr == 2
+end
+
+def triangle(side1, side2, side3)
+  sides = [side1, side2, side3]
+  max = sides.max
+  min = sides.min
+
+  return :invalid if !valid_triangle?(sides, max, min)
+  return :equilateral if equilateral?(sides, max)
+  return :isosceles if isosceles_or_scalene?(sides, max, min)
+  return :scalene if !isosceles_or_scalene?(sides, max, min)
 end
 
 # Examples:
