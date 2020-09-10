f786181a6ccbb29ca09dd129d6083e7954ea2f72
James Lafferty <james@j-lafferty.net>
2020-09-10 16:47:14 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_card_deck.rb
+++ rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_card_deck.rb
@@ -2,12 +2,14 @@
 
 # Once you get the program to run and produce a sum, you might notice that the sum is off: It's lower than it should be. Why is that?
 
+require 'pry'
+
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
 
-deck = { :hearts   => cards,
-         :diamonds => cards,
-         :clubs    => cards,
-         :spades   => cards }
+deck = { :hearts   => cards.clone,
+         :diamonds => cards.clone,
+         :clubs    => cards.clone,
+         :spades   => cards.clone }
 
 def score(card)
   case card
@@ -20,18 +22,21 @@ def score(card)
 end
 
 # Pick one random card per suit
+# binding.pry
 
 player_cards = []
 deck.keys.each do |suit|
   cards = deck[suit]
   cards.shuffle!
   player_cards << cards.pop
+
+  # binding.pry
 end
 
 # Determine the score of the remaining cards in the deck
 
 sum = deck.reduce(0) do |sum, (_, remaining_cards)|
-  remaining_cards.map do |card|
+  remaining_cards.map! do |card|
     score(card)
   end
 
@@ -39,3 +44,8 @@ sum = deck.reduce(0) do |sum, (_, remaining_cards)|
 end
 
 puts sum
+
+total_sum  = 4 * [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11].sum
+player_sum = player_cards.map { |card| score(card) }.sum
+
+puts (sum == total_sum - player_sum) #=> true
