395ef765abaeb05e7c4873c2472f01dce2f7e74f
James Lafferty <james@j-lafferty.net>
2020-09-10 15:17:33 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_countdown.rb
+++ rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_countdown.rb
@@ -1,14 +1,17 @@
 # Our countdown to launch isn't behaving as expected. Why? Change the code so that our program successfully counts down from 10 to 1.
 
+# Further Exploration
+# We specify 10 two times, which looks a bit redundant. It should be possible to specify it only once. Can you refactor the code accordingly?
+
 def decrease(counter)
-  counter -= 1
+  counter - 1
 end
 
 counter = 10
 
-10.times do
+counter.times do
   puts counter
-  decrease(counter)
+  counter = decrease(counter)
 end
 
 puts 'LAUNCH!'
