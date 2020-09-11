011c3ed12374497f6afc782d3e303535c7390f17
James Lafferty <james@j-lafferty.net>
2020-09-10 17:38:01 -0700

    Add solution for further exploration

--- rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_getting_things_done.rb
+++ rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_getting_things_done.rb
@@ -1,8 +1,12 @@
 # We wrote a method for moving a given number of elements from one array to another. We decide to test it on our todo list, but invoking move on line 11 results in a SystemStackError. What does this error mean and why does it happen?
 
+# Further Exploration
+# What happens if the length of the from_array is smaller than n?
+# -- if from_array is smaller than n, then nil will be added appended to to_array
+
 def move(n, from_array, to_array)
   # need condition to terminate recursion
-  return if n == 0
+  return if n == 0 || from_array.empty?
   to_array << from_array.shift
   move(n - 1, from_array, to_array)
 end
@@ -12,7 +16,8 @@ end
 todo = ['study', 'walk the dog', 'coffee with Tom']
 done = ['apply sunscreen', 'go to the beach']
 
-move(2, todo, done)
+# move(2, todo, done)
+move(3, todo, done)
 
 p todo # should be: ['coffee with Tom']
 p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
