5c9cce1423dd65eaee9ffbeace277fb7a6494c15
James Lafferty <james@j-lafferty.net>
2020-09-11 14:00:38 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_whats_wrong_with_the_output.rb
+++ rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_whats_wrong_with_the_output.rb
@@ -1,9 +1,9 @@
 # Josh wants to print an array of numeric strings in reverse numerical order. However, the output is wrong. Without removing any code, help Josh get the expected output.
 
 arr = ["9", "8", "7", "10", "11"]
-p arr.sort do |x, y|
-    y.to_i <=> x.to_i
-  end
+
+# the do..end block causes return issues when calling p on arr.sort
+p arr.sort { |x, y| y.to_i <=> x.to_i }
 
 # Expected output: ["11", "10", "9", "8", "7"] 
 # Actual output: ["10", "11", "7", "8", "9"] 
