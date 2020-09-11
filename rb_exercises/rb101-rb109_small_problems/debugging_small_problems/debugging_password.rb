e0e242958d3a890b5e86d020be5eae3dd5aa4092
James Lafferty <james@j-lafferty.net>
2020-09-11 09:42:49 -0700

    Add solution

--- rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_password.rb
+++ rb_exercises/rb101-rb109_small_problems/debugging_small_problems/debugging_password.rb
@@ -7,10 +7,9 @@ password = nil
 def set_password
   puts 'What would you like your password to be?'
   new_password = gets.chomp
-  password = new_password
 end
 
-def verify_password
+def verify_password(password) # password needs to be passed here
   puts '** Login **'
   print 'Password: '
   input = gets.chomp
@@ -23,7 +22,7 @@ def verify_password
 end
 
 if !password
-  set_password
+  password = set_password # password needs to be assigned here
 end
 
-verify_password
+verify_password(password) # password needs to be passed here
